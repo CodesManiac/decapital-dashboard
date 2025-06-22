7dd5702fb57c7b3826df9f2156a303584f608816
Codesmaniac <joyajiboye2016@gmail.com>
2025-06-22 20:54:16 +0100

    fetch user and posts data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
+import { Post, User } from "@/types";
 import ExperienceTabs from "./components/ExperienceTabs";
 import Navbar from "./components/Navbar";
 import ProfileHeader from "./components/ProfileHeader";
 import Sidebar from "./components/Sidebar";
 
-export default function Home() {
+
+async function getUserData(): Promise<{ user: User; posts: Post[] }>{
+  const userRes = await fetch('https://jsonplaceholder.typicode.com/users/1', {
+    cache: 'no-store',
+  });
+  const postsRes = await fetch('https://jsonplaceholder.typicode.com/posts?userId=1', {
+    cache: 'no-store',
+  });
+
+  const user = await userRes.json();
+  const posts = await postsRes.json();
+
+  return { user, posts };
+}
+export default async function Home() {
+  const { user, posts } = await getUserData();
+  console.log(posts,"posts")
+  console.log(user,"user");
+  const experiences = posts.slice(0, 5).map((post, index) => ({
+    id: post.id,
+    title: post.title,
+    organization: user.company?.name || 'Org Name',
+    location: user.address?.city || 'City',
+    date: `202${index}`,
+    tags: ['Training', 'Church Singer'],
+  }));
   return (
     <div className="flex min-h-screen">
     <Sidebar />
